c06b9d6916d157fb283e254fc0814a40f132ee0e
Vojtak42 <Vojtak42@email.cz>
2024-05-24 19:04:20 +0200

    reverse

--- src/bakabot/utils/utils.py
+++ src/bakabot/utils/utils.py
@@ -53,7 +53,7 @@ async def login(client: disnake.Client):
     username = os_environ("bakalariUsername")
     password = os_environ("bakalariPassword")
 
-    url = "https://gymkh.bakalari.cz/login"
+    url = "https://bakalari.ceskolipska.cz/Login"
     data = {"username": username, "password": password, "returnUrl": "", "login": ""}
     session = aiohttp.ClientSession()
     try:
