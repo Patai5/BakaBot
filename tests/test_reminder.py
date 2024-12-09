8d78230b642b906926701cf9e4a33680fad8fdf5
Patai5 <lukyn.patai5@gmail.com>
2024-12-10 00:13:19 +0100

    test(unit): update lesson reminder tests

--- tests/test_reminder.py
+++ tests/test_reminder.py
@@ -1,9 +1,13 @@
-from core.reminder import REMIND_BEFORE_CLASS_TIME_SEC, REMIND_WHOLE_DAY_SCHEDULE_TIME, getWaitUntilNextRemindTime
+from core.reminder import (
+    REMIND_AFTER_PREVIOUS_CLASS_TIME_SEC,
+    REMIND_WHOLE_DAY_SCHEDULE_TIME,
+    getWaitUntilNextRemindTime,
+)
 from test_schedule import TestSchedules
 
 
 def test_remind_whole_day():
-    """Should remind about the whole day schedule before any classes"""
+    """Should remind about the whole day schedule with the first lesson"""
     schedule = TestSchedules.emptySchedule
     currentTimeSec = 50
 
@@ -12,20 +16,22 @@ def test_remind_whole_day():
     assert remindTime.remindWholeDaySchedule is True
 
 
-def test_remind_first_class():
-    """Should remind about the first class"""
+def test_remind_second_class():
+    """Should remind about the second class"""
     schedule = TestSchedules.only4thLessons
-    currentTimeSec = REMIND_WHOLE_DAY_SCHEDULE_TIME + 1
+    currentTimeSec = TestSchedules.defaultLessonTimes[0] + REMIND_AFTER_PREVIOUS_CLASS_TIME_SEC + 1
 
     remindTime = getWaitUntilNextRemindTime(schedule, currentTimeSec)
-    assert remindTime.timeSec == TestSchedules.defaultLessonTimes[0] - REMIND_BEFORE_CLASS_TIME_SEC - currentTimeSec
+    expectedLessonTime = TestSchedules.defaultLessonTimes[1] + REMIND_AFTER_PREVIOUS_CLASS_TIME_SEC - currentTimeSec
+
+    assert remindTime.timeSec == expectedLessonTime
     assert remindTime.remindWholeDaySchedule is False
 
 
 def test_should_go_over_to_next_day():
     """Should go over to the next day if there are no more lessons"""
     schedule = TestSchedules.only4thLessons
-    currentTimeSec = TestSchedules.defaultLessonTimes[-1] + 1
+    currentTimeSec = TestSchedules.defaultLessonTimes[-1]
 
     remindTime = getWaitUntilNextRemindTime(schedule, currentTimeSec)
     assert remindTime.timeSec == REMIND_WHOLE_DAY_SCHEDULE_TIME + 86400 - currentTimeSec
